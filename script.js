61b4d1d7651c48e067e649c37df3532e4488170b
whps40333 <whps40333@gmail.com>
2023-10-31 15:41:57 +0800

    finish day_24 pratice

---
+++ script.js
@@ -0,0 +1,29 @@
+const nav = document.querySelector("#main");
+// const nav = document.querySelector("#main");
+
+// 取得選單 top
+topOfNav = nav.offsetTop;
+//  topOfNav = nav.offsetTop;
+
+// 修正選單
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    // 增加內文 padding 避免內文因為凍結標題列而被遮蔽
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.classList.remove("fixed-nav");
+    document.body.style.paddingTop = 0;
+  }
+}
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.classList.remove("fixed-nav");
+  }
+}
+
+// 建立卷軸移動事件
+window.addEventListener("scroll", fixNav);
